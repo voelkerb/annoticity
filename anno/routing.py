6f8583b93139a80952482528f1aa59b7c321e8a0
Benjamin Voelker <voelkerb@informatik.uni-freiburg.de>
2020-09-09 18:26:16 +0200

    bugfix

--- anno/routing.py
+++ anno/routing.py
@@ -3,7 +3,7 @@ from . import websocket
 from django.urls import re_path
 
 websocket_urlpatterns = [
-    re_path(r'ws/', websocket.SocketConsumer),
-    re_path(r'/ben/ws/', websocket.SocketConsumer),
+    re_path(r'anno/ws', websocket.SocketConsumer),
+    re_path(r'ben/ws', websocket.SocketConsumer),
 ]
 
