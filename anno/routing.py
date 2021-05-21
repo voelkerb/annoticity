3c51de122e47b30f81afaad1862ef92c0e927976
Benjamin Voelker <voelkerb@informatik.uni-freiburg.de>
2021-05-21 10:28:29 +0200

    comply to channels v3 websockets

--- anno/routing.py
+++ anno/routing.py
@@ -3,7 +3,7 @@ from . import websocket
 from django.urls import re_path
 
 websocket_urlpatterns = [
-    re_path(r'anno/ws', websocket.SocketConsumer),
-    re_path(r'ben/ws', websocket.SocketConsumer),
+    re_path(r'anno/ws', websocket.SocketConsumer.as_asgi()),
+    re_path(r'ben/ws', websocket.SocketConsumer.as_asgi()),
 ]
 
--- annoticity/routing.py
+++ annoticity/routing.py
@@ -1,7 +1,9 @@
 # mysite/routing.py
+from anno import websocket
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import anno.routing
+from django.urls import path
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
